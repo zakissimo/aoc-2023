4203c21f1d9a0153d0afffac9df9608912f45917
zakissimo <zakaria.habri@gmail.com>
2023-12-10 18:10:42 +0100

    Solved Day 5 (Optimized version)

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -5,24 +5,24 @@ use std::fs::read_to_string;
 
 struct Almanac<'a> {
     keys: Vec<(&'a str, &'a str)>,
-    seeds: Vec<usize>,
+    seeds: Vec<u64>,
     _elements: HashSet<&'a str>,
-    map: HashMap<(&'a str, &'a str), Vec<Vec<usize>>>,
+    map: HashMap<(&'a str, &'a str), Vec<Vec<u64>>>,
 }
 
 fn parse(input: &String) -> Almanac {
-    let mut seeds = Vec::<usize>::new();
+    let mut seeds = Vec::<u64>::new();
 
     let mut _elements = HashSet::<&str>::new();
     let mut keys = Vec::<(&str, &str)>::new();
-    let mut map = HashMap::<(&str, &str), Vec<Vec<usize>>>::new();
+    let mut map = HashMap::<(&str, &str), Vec<Vec<u64>>>::new();
 
     for line in input.lines() {
         if line.contains("seeds: ") {
             if let Some((_, right)) = line.split_once(' ') {
                 seeds = right
                     .split_whitespace()
-                    .map(|e| e.parse::<usize>().unwrap_or(0))
+                    .map(|e| e.parse::<u64>().unwrap_or(0))
                     .collect();
             }
         }
@@ -33,7 +33,7 @@ fn parse(input: &String) -> Almanac {
                     _elements.insert(right);
                     _elements.insert(left);
                     keys.push((left, right));
-                    map.insert((left, right), Vec::<Vec<usize>>::new());
+                    map.insert((left, right), Vec::<Vec<u64>>::new());
                 }
             }
         } else if !line.is_empty() {
@@ -57,8 +57,8 @@ fn parse(input: &String) -> Almanac {
     }
 }
 
-fn part_one(almanac: &Almanac) -> Result<usize, Box<dyn Error>> {
-    let mut ans = usize::MAX;
+fn part_one(almanac: &Almanac) -> Result<u64, Box<dyn Error>> {
+    let mut ans = u64::MAX;
 
     for seed in &almanac.seeds {
         let mut queue = vec![*seed];
@@ -80,53 +80,57 @@ fn part_one(almanac: &Almanac) -> Result<usize, Box<dyn Error>> {
     Ok(ans)
 }
 
-fn ranges_to_ranges(v: &Vec<Vec<usize>>, c: &Vec<(usize, usize)>) -> Vec<(usize, usize)> {
+fn ranges_to_ranges(maps: &Vec<Vec<u64>>, ranges: &Vec<(u64, u64)>) -> Vec<(u64, u64)> {
+    let mut out = Vec::<(u64, u64)>::new();
 
-    let mut out = Vec::<(usize, usize)>::new();
+    for range in ranges {
+        let mut ret = Vec::<(u64, u64)>::new();
+        for map in maps {
+            let map_a = map[1];
+            let map_b = map[1] + map[2];
 
-    for curr in c {
-        let mut ret = Vec::<(usize, usize)>::new();
-        for vec in v {
-            if (vec[1]..(vec[1] + vec[2])).contains(&curr.0) {
-                ret.push((vec[0] + curr.0 - vec[1], vec[2].min(curr.1)));
-                if curr.0 + curr.1 > vec[1] + vec[2] {
-                    ret.push((
-                        curr.0 + vec[2].min(curr.1),
-                        (curr.0 + curr.1) - (vec[1] + vec[2]),
-                    ));
-                }
-            } else if (vec[1]..(vec[1] + vec[2])).contains(&(curr.0 + curr.1)) {
-                ret.push((curr.0.max(vec[1]) + vec[0] - vec[1], vec[2].min(curr.1)));
-                if curr.0 < vec[1] {
-                    ret.push((curr.0.min(vec[1]), curr.0.max(vec[1])))
+            let range_a = range.0;
+            let range_b = range.0 + range.1;
+            let range_len = range.1;
+
+            if (map_a..map_b).contains(&range_a) && (map_a..map_b).contains(&range_b) {
+                let new_range_a = range_a - map[1] + map[0];
+                ret.push((new_range_a, range_len));
+            } else if (map_a..map_b).contains(&range_a) {
+                let new_range_a = range_a - map[1] + map[0];
+                ret.push((new_range_a, range_len - (range_b - map_b)));
+                ret.extend(ranges_to_ranges(maps, &vec![(map_b + 1, range_b - map_b)].to_vec()));
+            } else if (map_a..map_b).contains(&range_b) {
+                let new_map_a = map_a - map[1] + map[0];
+                ret.push((new_map_a, range_b - map_a));
+                if range_a != 0 {
+                    ret.extend(ranges_to_ranges(maps, &vec![(range_a - 1, map_a - range_a)].to_vec()));
                 }
             }
         }
         if ret.is_empty() {
-            ret.push(*curr);
+            ret.push(*range);
         }
         out.extend(ret);
     }
     out
 }
 
-fn part_two(almanac: &Almanac) -> usize {
-    let mut ans = Vec::<(usize, usize)>::new();
+fn part_two(almanac: &Almanac) -> u64 {
+    let mut ans = Vec::<(u64, u64)>::new();
 
     for seed_pair in almanac.seeds.chunks(2) {
-        let mut queue = vec![(seed_pair[0], seed_pair[1])].to_vec();
+        let mut seed_vec = vec![(seed_pair[0], seed_pair[1])].to_vec();
         for key in &almanac.keys {
-            if let Some(v) = almanac.map.get(&key) {
-                queue = ranges_to_ranges(&v, &queue);
+            if let Some(maps) = almanac.map.get(&key) {
+                seed_vec = ranges_to_ranges(&maps, &seed_vec);
             }
         }
-        let min_queue = queue.iter().min_by_key(|&(left, _)| left).unwrap();
-            ans.push(min_queue.clone());
+        let min_queue = seed_vec.iter().min_by_key(|&(left, _)| left).unwrap();
+        ans.push(min_queue.clone());
     }
 
-    println!("{:?}", ans);
-
-    let min = &ans.iter().min_by_key(|&(left, _)| left);
+    let min = &ans.iter().filter(|&&(left, _)| left != 0).min_by_key(|&(left, _)| left);
 
     match min {
         Some(ret) => ret.0,
