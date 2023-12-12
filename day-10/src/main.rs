3f87bd1224f3f7efe01567a7246f9ac1424eca82
zakissimo <zakaria.habri@gmail.com>
2023-12-12 23:22:17 +0100

    WIP for Day 10

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -2,6 +2,30 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::fs::read_to_string;
 
+#[derive(Debug, Copy, Clone)]
+struct Dir {
+    dx: i32,
+    dy: i32,
+}
+
+impl Dir {
+    const UP: Dir = Dir { dy: -1, dx: 0 };
+    const DOWN: Dir = Dir { dy: 1, dx: 0 };
+    const LEFT: Dir = Dir { dy: 0, dx: -1 };
+    const RIGHT: Dir = Dir { dy: 0, dx: 1 };
+    const LIST: [Dir; 4] = [Dir::UP, Dir::DOWN, Dir::LEFT, Dir::RIGHT];
+
+    fn from_tuple(tuple: (i32, i32)) -> Option<Dir> {
+        match tuple {
+            (0, -1) => Some(Dir::UP),
+            (0, 1) => Some(Dir::DOWN),
+            (-1, 0) => Some(Dir::LEFT),
+            (1, 0) => Some(Dir::RIGHT),
+            _ => None,
+        }
+    }
+}
+
 fn parse(input: &str) -> Result<Vec<&[u8]>, Box<dyn Error>> {
     Ok(input.lines().map(|line| line.as_bytes()).collect())
 }
@@ -15,30 +39,8 @@ fn is_inbound(grid: &Vec<&[u8]>, x: usize, y: usize) -> bool {
     false
 }
 
-fn get_next(
-    dirs: &[(i32, i32); 2],
-    grid: &Vec<&[u8]>,
-    prev: &mut (usize, usize),
-    curr: &mut (usize, usize),
-) -> Option<(usize, usize)> {
-    let diff: (i32, i32) = (curr.0 as i32 - prev.0 as i32, curr.1 as i32 - prev.1 as i32);
-    for dir in dirs {
-        if *dir != diff
-            && is_inbound(
-                grid,
-                (curr.0 as i32 + dir.0) as usize,
-                (curr.1 as i32 + dir.1) as usize,
-            )
-        {
-            *prev = *curr;
-            let next = (
-                (curr.0 as i32 + dir.0) as usize,
-                (curr.1 as i32 + dir.1) as usize,
-            );
-            return Some(next);
-        }
-    }
-    None
+fn is_pipe(c: u8) -> bool {
+    return "|-LJ7F".contains(c as char);
 }
 
 fn get_start(grid: &Vec<&[u8]>) -> Option<(usize, usize)> {
@@ -53,68 +55,43 @@ fn get_start(grid: &Vec<&[u8]>) -> Option<(usize, usize)> {
     None
 }
 
-fn walk(grid: &Vec<&[u8]>, start: (usize, usize)) -> usize {
-    let pipe_list = vec![b'|', b'-', b'L', b'J', b'7', b'F'];
-    let pipes: HashMap<u8, [(i32, i32); 2]> = HashMap::from([
-        (b'|', [(1, 0), (-1, 0)]),
-        (b'-', [(0, -1), (0, 1)]),
-        (b'L', [(1, 0), (0, 1)]),
-        (b'J', [(1, 0), (0, -1)]),
-        (b'7', [(-1, 0), (0, -1)]),
-        (b'F', [(-1, 0), (0, 1)]),
+fn walk(grid: &Vec<&[u8]>, prev: &mut (usize, usize), curr: &mut (usize, usize)) {
+    let mut visited: Vec<Vec<bool>> = grid.iter().map(|row| vec![false; row.len()]).collect();
+    let pipes: HashMap<u8, [Dir; 2]> = HashMap::from([
+        (b'|', [Dir::UP, Dir::DOWN]),
+        (b'-', [Dir::LEFT, Dir::RIGHT]),
+        (b'L', [Dir::UP, Dir::RIGHT]),
+        (b'J', [Dir::UP, Dir::LEFT]),
+        (b'7', [Dir::DOWN, Dir::LEFT]),
+        (b'F', [Dir::DOWN, Dir::RIGHT]),
     ]);
-    let dirs: [(i32, i32); 4] = [(1, 0), (-1, 0), (0, 1), (0, -1)];
 
-    let mut ways = Vec::<(usize, usize)>::new();
-    for dir in dirs {
-        if is_inbound(
-            grid,
-            (start.0 as i32 + dir.0) as usize,
-            (start.1 as i32 + dir.1) as usize,
-        ) {
-            ways.push((
-                (start.0 as i32 + dir.0) as usize,
-                (start.1 as i32 + dir.1) as usize,
-            ));
-        }
-    }
-    println!("> {:?}", ways);
-    let mut ret = 0;
-    println!("{:?}", grid);
-    for way in ways {
-        let mut prev = start;
-        let mut curr = way;
-        let mut ans = 0;
-        if pipe_list.contains(&grid[curr.0][curr.1]) {
-            while let Some(next) = get_next(
-                pipes.get(&grid[curr.0][curr.1]).unwrap(),
-                grid,
-                &mut prev,
-                &mut curr,
-            ) {
-                println!("{}", grid[next.0][next.1]);
-                if grid[next.0][next.1] == b'S' || ans == 5 {
-                    break;
-                }
-                ans += 1;
-            }
-        }
-        ret = ans;
-    }
-    ret
+    let diff: (i32, i32) = (curr.0 as i32 - prev.0 as i32, curr.1 as i32 - prev.1 as i32);
+    if let Some(dir) = Dir::from_tuple(diff) {}
 }
 
 fn part_one(input: &str) -> Result<usize, Box<dyn Error>> {
-    let mut ans = 0;
+    // let mut ans = 0;
     let grid = parse(input)?;
 
     println!("{:?}", grid);
     if let Some(start) = get_start(&grid) {
+        let mut ways = Vec::<(usize, usize)>::new();
+        for dir in Dir::LIST {
+            let x = (start.0 as i32 + dir.dx) as usize;
+            let y = (start.1 as i32 + dir.dy) as usize;
+            if is_inbound(&grid, x, y) && is_pipe(grid[y][x]) {
+                ways.push((x, y));
+            }
+        }
+
+        for (x, y) in &ways {}
+
+        println!("{:?}", ways);
         println!("{:?}", start);
-        ans = walk(&grid, start);
     }
 
-    Ok(ans)
+    Ok(0)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
