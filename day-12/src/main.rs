81fc951f1cb4da0f9c3a5e057623bdedec800ebf
zakissimo <zakaria.habri@gmail.com>
2023-12-15 21:15:44 +0100

    Init Day 12

---
+++ day-12/src/main.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+use std::fs::read_to_string;
+
+fn parse(input: &str) -> Vec<(&str, Vec<usize>)> {
+    input.lines().map(|row| {
+        if let Some((left, right)) = row.split_once(' ') {
+            (left, right.split(',').map(|e| e.parse::<usize>().expect("Input should be digits only")).collect())
+        } else {
+            panic!("Input isn't formatted correctly!");
+        }
+    }).collect()
+}
+
+fn part_one(input: &str) {
+    let input = parse(input);
+    println!("{:?}", input);
+}
+
+fn main() -> Result<()> {
+    let sample = read_to_string("sample")?;
+    // let input = read_to_string("input");
+
+    println!("{:?}", sample);
+    part_one(&sample);
+    Ok(())
+}
