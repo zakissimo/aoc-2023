fb2babb98d3fcebd7fd35be3c1de7db7357128e7
zakissimo <zakaria.habri@gmail.com>
2023-12-17 09:31:06 +0100

    Better parsing for Day 12

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,14 +1,23 @@
 use anyhow::Result;
 use std::fs::read_to_string;
 
-fn parse(input: &str) -> Vec<(&str, Vec<usize>)> {
-    input.lines().map(|row| {
-        if let Some((left, right)) = row.split_once(' ') {
-            (left, right.split(',').map(|e| e.parse::<usize>().expect("Input should be digits only")).collect())
-        } else {
-            panic!("Input isn't formatted correctly!");
-        }
-    }).collect()
+fn parse(input: &str) -> Vec<(Vec<&str>, Vec<usize>)> {
+    input
+        .lines()
+        .map(|row| {
+            if let Some((left, right)) = row.split_once(' ') {
+                (
+                    left.split('.').filter(|e| !e.is_empty()).collect(),
+                    right
+                        .split(',')
+                        .map(|e| e.parse::<usize>().expect("Input should be digits only"))
+                        .collect(),
+                )
+            } else {
+                panic!("Input isn't formatted correctly!");
+            }
+        })
+        .collect()
 }
 
 fn part_one(input: &str) {
@@ -18,9 +27,10 @@ fn part_one(input: &str) {
 
 fn main() -> Result<()> {
     let sample = read_to_string("sample")?;
-    // let input = read_to_string("input");
+    // let input = read_to_string("input")?;
 
     println!("{:?}", sample);
     part_one(&sample);
+    // part_one(&input);
     Ok(())
 }
